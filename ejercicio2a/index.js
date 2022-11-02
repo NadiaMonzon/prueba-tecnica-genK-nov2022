d008b9b558cda9c37c7fd860140f7e03e74237be
NadiaMonzon <nadiamonque99@gmail.com>
2022-11-02 18:18:32 +0000

    feat: get the result of the operations of the arrays

--- ejercicio2a/index.js
+++ ejercicio2a/index.js
@@ -6,8 +6,19 @@ const array1 = [
     [5, 3, 1],
   ];
   
-  const array2 = [
+
+const array2 = [
     [2, 4, 6],
     [8, 10, 8],
     [6, 4, 2],
   ];
+
+  function getTheRowsSolutions (){
+    const firstResult = array1[0][0] * array2[0][0] + array1[0][1] * array2[1][0] + array1[0][2] * array2[2][0];
+    const secondResult = array1[1][0] * array2[0][1] + array1[1][1] * array2[1][1] + array1[1][2] * array2[2][1];
+    const thirdResult = array1[2][0] * array2[0][2] + array1[2][1] * array2[1][2] + array1[2][2] * array2[2][2];
+
+    const result = [firstResult, secondResult, thirdResult]
+    return result;
+  }
+
